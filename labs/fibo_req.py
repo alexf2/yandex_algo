73162ad078dbbc027bdae93da3a2b3eb2d745a4b
Alexey Fedorov <Aleksey-Fedorov@mailfrom.ru>
2024-12-09 01:15:25 +0300

    Fibo

---
+++ labs/fibo_req.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def find_fibo_n(n):
+    if n < 2:
+        return 1
+
+    return find_fibo_n(n - 2) + find_fibo_n(n - 1)
+
+
+def main():
+    n = int(sys.stdin.readline().strip())
+
+    print(find_fibo_n(n))
+
+
+if __name__ == '__main__':
+    main()
